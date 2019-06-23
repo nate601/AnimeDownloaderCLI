de29abbe7c790400ca4f383ae411c447d3e4057c
Nathan Button <nathanebutton@gmail.com>
2019-06-23 00:30:29 -0500

    Ensure that code formatting is standardized

--- DownloadHandler.cs
+++ DownloadHandler.cs
@@ -3,79 +3,57 @@ using System.Collections;
 using System.Collections.Generic;
 using SimpleIRCLib;
 
-namespace AnimeDown
-{
-    public class DownloadHandler
-    {
+namespace AnimeDown {
+    public class DownloadHandler {
         private readonly SimpleIRC irc;
-        readonly Queue<DownloadPair> downloadQueue = new Queue<DownloadPair>();
+        readonly Queue<DownloadPair> downloadQueue = new Queue<DownloadPair> ();
         private bool firstRun = true;
-        public DownloadHandler()
-        {
-            irc = new SimpleIRC();
-            irc.SetupIrc("irc.rizon.net", "animeguy69" + new Random().Next(70), "#horriblesubs", 6697);
-
+        public DownloadHandler () {
+            irc = new SimpleIRC ();
+            irc.SetupIrc ("irc.rizon.net", "animeguy69" + new Random ().Next (70), "#horriblesubs", 6697);
             irc.DccClient.OnDccEvent += DownloadStatusChanged;
-            // Grant Poquiz
-            // Changes Download location to the application path instead of a new folder
-            irc.SetCustomDownloadDir(System.IO.Directory.GetCurrentDirectory());
-            irc.StartClient();
+            irc.SetCustomDownloadDir (System.IO.Directory.GetCurrentDirectory ());
+            irc.StartClient ();
+        }
+        private void DownloadStatusChanged (object sender, DCCEventArgs args) {
+            Console.Clear ();
+            Console.WriteLine ($"Current File: {args.FileName} ");
+            Console.WriteLine ($"{args.Progress}%");
+            foreach (var downloadPair in downloadQueue) {
+                Console.WriteLine ($"{downloadPair.BotName} : {downloadPair.PackNumber}");
+            }
+            if (args.Status == "COMPLETED" && downloadQueue.Count != 0) {
+                var next = downloadQueue.Dequeue ();
+                SendToDownloadbot (next.BotName, next.PackNumber);
+            }
+            if (args.Status == "COMPLETED" && downloadQueue.Count == 0) {
+                Console.WriteLine ("Download completed. Press any key to exit.");
+            }
+        }
+        public void SendToDownloadbot (string botName, int packNumber) {
+            irc.SendMessageToChannel ($@"/msg {botName} xdcc send {packNumber.ToString()}", "#horriblesubs");
         }
 
-        private void DownloadStatusChanged(object sender, DCCEventArgs args)
-        {
-            Console.Clear();
-            Console.WriteLine($"Current File: {args.FileName} ");
-            Console.WriteLine($"{args.Progress}%");
-            foreach (var downloadPair in downloadQueue)
-            {
-                Console.WriteLine($"{downloadPair.BotName} : {downloadPair.PackNumber}");
-            }
-
-
-            if (args.Status == "COMPLETED" && downloadQueue.Count != 0)
-            {
-                var next = downloadQueue.Dequeue();
-                SendToDownloadbot(next.BotName, next.PackNumber);
-            }
-
-            if (args.Status == "COMPLETED" && downloadQueue.Count == 0)
-            {
-                Console.WriteLine("Download completed. Press any key to exit.");
-            }
-
-
-        }
-        public void SendToDownloadbot(string botName, int packNumber)
-        {
-            irc.SendMessageToChannel($@"/msg {botName} xdcc send {packNumber.ToString()}", "#horriblesubs");
-        }
-
-        public void Download(string botName, int packNumber)
-        {
-            downloadQueue.Enqueue(new DownloadPair(botName, packNumber));
-            if (firstRun)
-            {
-                var next = downloadQueue.Dequeue();
-                SendToDownloadbot(next.BotName, next.PackNumber);
+        public void Download (string botName, int packNumber) {
+            downloadQueue.Enqueue (new DownloadPair (botName, packNumber));
+            if (firstRun) {
+                var next = downloadQueue.Dequeue ();
+                SendToDownloadbot (next.BotName, next.PackNumber);
                 firstRun = false;
             }
         }
 
-        public struct DownloadPair
-        {
+        public struct DownloadPair {
             public string BotName { get; set; }
             public int PackNumber { get; set; }
 
-            public DownloadPair(string botName, int packNumber)
-            {
+            public DownloadPair (string botName, int packNumber) {
                 this.BotName = botName;
                 this.PackNumber = packNumber;
             }
         }
-        public void SetDownloadDirectory(string path)
-        {
-            irc.SetCustomDownloadDir(path);
+        public void SetDownloadDirectory (string path) {
+            irc.SetCustomDownloadDir (path);
         }
     }
 }
--- HorribleSubs.cs
+++ HorribleSubs.cs
@@ -1,93 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Net;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text.RegularExpressions;
 
-public class HorribleSubsPacklist
-{
-    public List<ShowEntry> GetShow(string title, Quality quality )
-    {
+public class HorribleSubsPacklist {
+    public List<ShowEntry> GetShow (string title, Quality quality) {
         string url = "https://xdcc.horriblesubs.info/search.php" + "?t=" + title + "";
-        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+        HttpWebRequest req = (HttpWebRequest) WebRequest.Create (url);
         req.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-
 
         req.Referer = @"https://xdcc.horriblesubs.info/";
         req.Method = "GET";
-       
 
-
-        HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
-        int responseCode = (int)resp.StatusCode;
-        Console.WriteLine("Sending 'GET' request to URL : " + url);
-        Console.WriteLine("Response Code : " + responseCode);
+        HttpWebResponse resp = (HttpWebResponse) req.GetResponse ();
+        int responseCode = (int) resp.StatusCode;
+        Console.WriteLine ("Sending 'GET' request to URL : " + url);
+        Console.WriteLine ("Response Code : " + responseCode);
 
         string response;
-        using (StreamReader input = new StreamReader(resp.GetResponseStream()))
-        {
-            response = input.ReadToEnd();
+        using (StreamReader input = new StreamReader (resp.GetResponseStream ())) {
+            response = input.ReadToEnd ();
         }
 
-        List<ShowEntry> shows = new List<ShowEntry>();
-        foreach (var s in response.Split(Environment.NewLine.ToCharArray()))
-        {
-            switch (quality)
-            {
+        List<ShowEntry> shows = new List<ShowEntry> ();
+        foreach (var s in response.Split (Environment.NewLine.ToCharArray ())) {
+            switch (quality) {
                 case Quality.TEN_EIGHTY_P:
-                    if (s.Contains("1080p"))
-                    {
-                        shows.Add(new ShowEntry(s));
+                    if (s.Contains ("1080p")) {
+                        shows.Add (new ShowEntry (s));
                     }
                     break;
                 case Quality.SEVEN_TWENTY_P:
-                    if (s.Contains("720p"))
-                    {
-                        shows.Add(new ShowEntry(s));
+                    if (s.Contains ("720p")) {
+                        shows.Add (new ShowEntry (s));
                     }
                     break;
                 case Quality.STANDARD_DEFINITION:
-                    if(!s.Contains("1080p") && !s.Contains("720p"))
-                        shows.Add(new ShowEntry(s));
+                    if (!s.Contains ("1080p") && !s.Contains ("720p"))
+                        shows.Add (new ShowEntry (s));
                     break;
             }
         }
 
-        var showVerified = new List<ShowEntry>();
-        foreach (var showEntry in shows)
-        {
-            if(showEntry.Verify())
-                showVerified.Add(showEntry);
+        var showVerified = new List<ShowEntry> ();
+        foreach (var showEntry in shows) {
+            if (showEntry.Verify ())
+                showVerified.Add (showEntry);
         }
 
         return showVerified;
-        
+
     }
 
-    public class ShowEntry
-    {
+    public class ShowEntry {
 
-        
-
-        public static int GetTotalNumberOfEpisodes(List<ShowEntry> shows)
-        {
+        public static int GetTotalNumberOfEpisodes (List<ShowEntry> shows) {
             int episodeNumbers = 0;
-            foreach (var showEntry in shows)
-            {
-                if (int.Parse(showEntry.episodeNumber) > episodeNumbers)
-                {
-                    episodeNumbers = int.Parse(showEntry.episodeNumber);
+            foreach (var showEntry in shows) {
+                if (int.Parse (showEntry.episodeNumber) > episodeNumbers) {
+                    episodeNumbers = int.Parse (showEntry.episodeNumber);
                 }
             }
 
             return episodeNumbers;
         }
 
-        public static string GetShowName(List<ShowEntry> shows)
-        {
-            return shows.First().PrettyTitle();
+        public static string GetShowName (List<ShowEntry> shows) {
+            return shows.First ().PrettyTitle ();
         }
         public string botName;
         public string packNumber;
@@ -95,53 +77,47 @@ public class HorribleSubsPacklist
         public string Title;
         public string episodeNumber;
 
-        public ShowEntry(string lineEntry)
-        {
+        public ShowEntry (string lineEntry) {
 
-            Regex nameMatcher = new Regex("(?:b:\"|f:\")([^\"]*)"); //Match 0 group 1 = bot name
-                                                          //match 1 group 1 = show name
+            Regex nameMatcher = new Regex ("(?:b:\"|f:\")([^\"]*)"); //Match 0 group 1 = bot name
+            //match 1 group 1 = show name
 
-            var nameMatches = nameMatcher.Match(lineEntry);
+            var nameMatches = nameMatcher.Match (lineEntry);
             botName = nameMatches.Groups[1].Value;
-            Title = nameMatches.NextMatch().Groups[1].Value;
-            Regex packNumberMatcher = new Regex(@"n:(\d*)");
-            var packnumberMatches = packNumberMatcher.Match(lineEntry);
-            packNumber = packnumberMatches.Groups[1].ToString();
-            Regex sizeNumberMatcher = new Regex(@"s:(\d*)");
-            var sizeNumberMatches = sizeNumberMatcher.Match(lineEntry);
+            Title = nameMatches.NextMatch ().Groups[1].Value;
+            Regex packNumberMatcher = new Regex (@"n:(\d*)");
+            var packnumberMatches = packNumberMatcher.Match (lineEntry);
+            packNumber = packnumberMatches.Groups[1].ToString ();
+            Regex sizeNumberMatcher = new Regex (@"s:(\d*)");
+            var sizeNumberMatches = sizeNumberMatcher.Match (lineEntry);
             sizeInMb = sizeNumberMatches.Groups[1].Value;
-            Regex episodeNumberMatcher = new Regex(@"(?:- )(\d*)");
-            var regexNumberMatches = episodeNumberMatcher.Match(Title);
+            Regex episodeNumberMatcher = new Regex (@"(?:- )(\d*)");
+            var regexNumberMatches = episodeNumberMatcher.Match (Title);
             episodeNumber = regexNumberMatches.Groups[1].Value;
-            
+
         }
 
-        public bool Verify()
-        {
+        public bool Verify () {
             return botName != "" && packNumber != "" && sizeInMb != "" && Title != "" && episodeNumber != "";
         }
-        public string PrettyTitle()
-        {
-            Regex titlePrettifier = new Regex("(.+)(?: -)");
-            return titlePrettifier.Match(Title).Groups[1].Value.Split(']')[1].TrimStart(' ');
+        public string PrettyTitle () {
+            Regex titlePrettifier = new Regex ("(.+)(?: -)");
+            return titlePrettifier.Match (Title).Groups[1].Value.Split (']') [1].TrimStart (' ');
         }
 
-        public void PrettyPrint()
-        {
-            Console.WriteLine(PrettyTitle());
-            Console.WriteLine("├─" + episodeNumber);
-            Console.WriteLine("├─" + botName);
-            Console.WriteLine("├─" + packNumber);
-            Console.WriteLine("└─" + sizeInMb + " megabytes");
+        public void PrettyPrint () {
+            Console.WriteLine (PrettyTitle ());
+            Console.WriteLine ("├─" + episodeNumber);
+            Console.WriteLine ("├─" + botName);
+            Console.WriteLine ("├─" + packNumber);
+            Console.WriteLine ("└─" + sizeInMb + " megabytes");
         }
     }
-    public enum Quality
-    {
+    public enum Quality {
         TEN_EIGHTY_P,
         SEVEN_TWENTY_P,
         STANDARD_DEFINITION,
 
     }
-
 
 }
