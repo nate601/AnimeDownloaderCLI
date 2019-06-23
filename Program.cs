a725415e52e17ef8136e54f00cc42d3d7ed030ca
Nathan Button <nate601@live.com>
2019-06-23 00:30:29 -0500

    Ensure that code formatting is standardized

--- Program.cs
+++ Program.cs
@@ -1,43 +1,35 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using SimpleIRCLib;
 
-namespace AnimeDown
-{
-    class Program
-    {
+namespace AnimeDown {
+    class Program {
         private static bool verboseLogging = false;
-        private static readonly DownloadHandler handler = new DownloadHandler();
+        private static readonly DownloadHandler handler = new DownloadHandler ();
 
         private static bool hasBegunDownload = false;
 
-        static void Main(string[] args)
-        {
+        static void Main (string[] args) {
             if (args.Length > 0 && args[0] == "-l")
                 verboseLogging = true;
 
-
-            Console.WriteLine("Anime Downloader");
-            Console.Write("Anime Title:");
-            var animeTitle = Console.ReadLine();
-        resolutionPrompt:
-            Console.WriteLine("Resolution:");
-            Console.WriteLine("0: 1080p");
-            Console.WriteLine("1: 720p");
-            Console.WriteLine("2: SD");
-            // 12/19/2018
-            // Grant Poquiz
-            // changed response for reading in quality to be the same as the response for range
-            var response = Console.ReadKey(true);
+            Console.WriteLine ("Anime Downloader");
+            Console.Write ("Anime Title:");
+            var animeTitle = Console.ReadLine ();
+            resolutionPrompt:
+                Console.WriteLine ("Resolution:");
+            Console.WriteLine ("0: 1080p");
+            Console.WriteLine ("1: 720p");
+            Console.WriteLine ("2: SD");
+            var response = Console.ReadKey (true);
             HorribleSubsPacklist.Quality resolutionEnum;
-            switch (response.Key)
-            {
+            switch (response.Key) {
                 case ConsoleKey.D0:
                 case ConsoleKey.NumPad0:
                     resolutionEnum = HorribleSubsPacklist.Quality.TEN_EIGHTY_P;
@@ -54,215 +46,162 @@ namespace AnimeDown
                     goto resolutionPrompt;
             }
 
+            Console.WriteLine ($"Searching for {animeTitle} on horribleSubs");
+            HorribleSubsPacklist horrible = new HorribleSubsPacklist ();
+            var shows = horrible.GetShow (animeTitle, resolutionEnum);
 
-
-            Console.WriteLine($"Searching for {animeTitle} on horribleSubs");
-            HorribleSubsPacklist horrible = new HorribleSubsPacklist();
-            var shows = horrible.GetShow(animeTitle, resolutionEnum);
-
-        //foreach (var showEntry in shows)
-        //{
-        //    showEntry.PrettyPrint();
-        //}
-
-        prompt:
-            Console.WriteLine($"There are {HorribleSubsPacklist.ShowEntry.GetTotalNumberOfEpisodes(shows)} episodes of {HorribleSubsPacklist.ShowEntry.GetShowName(shows)} out.");
-            Console.WriteLine("Would you like to download (a)ll of them, (s)ome of them, or (o)ne of them?");
-            response = Console.ReadKey(true);
-            switch (response.Key)
-            {
+            prompt:
+                Console.WriteLine ($"There are {HorribleSubsPacklist.ShowEntry.GetTotalNumberOfEpisodes(shows)} episodes of {HorribleSubsPacklist.ShowEntry.GetShowName(shows)} out.");
+            Console.WriteLine ("Would you like to download (a)ll of them, (s)ome of them, or (o)ne of them?");
+            response = Console.ReadKey (true);
+            switch (response.Key) {
                 case ConsoleKey.A:
-                    DownloadAllPrompt(shows);
+                    DownloadAllPrompt (shows);
                     break;
                 case ConsoleKey.S:
-                    DownloadSomePrompt(shows);
+                    DownloadSomePrompt (shows);
 
                     break;
                 case ConsoleKey.O:
-                    DownloadOnePrompt(shows);
+                    DownloadOnePrompt (shows);
 
                     break;
                 default:
                     goto prompt;
             }
 
-            Console.ReadKey();
-
+            Console.ReadKey ();
 
         }
 
-        public static void DownloadAllPrompt(List<HorribleSubsPacklist.ShowEntry> shows)
-        {
-            Console.WriteLine($"Downloading all {HorribleSubsPacklist.ShowEntry.GetTotalNumberOfEpisodes(shows)} episodes!");
-            List<HorribleSubsPacklist.ShowEntry> showOptions = new List<HorribleSubsPacklist.ShowEntry>();
+        public static void DownloadAllPrompt (List<HorribleSubsPacklist.ShowEntry> shows) {
+            Console.WriteLine ($"Downloading all {HorribleSubsPacklist.ShowEntry.GetTotalNumberOfEpisodes(shows)} episodes!");
+            List<HorribleSubsPacklist.ShowEntry> showOptions = new List<HorribleSubsPacklist.ShowEntry> ();
 
-            List<string> botNames = new List<string>();
-            foreach (var showEntry in shows)
-            {
-                if (!botNames.Contains(showEntry.botName))
-                    botNames.Add(showEntry.botName);
+            List<string> botNames = new List<string> ();
+            foreach (var showEntry in shows) {
+                if (!botNames.Contains (showEntry.botName))
+                    botNames.Add (showEntry.botName);
             }
 
-            for (int botIndex = 0; botIndex < botNames.Count; botIndex++)
-            {
-                Console.WriteLine(botIndex + " : " + botNames[botIndex]);
+            for (int botIndex = 0; botIndex < botNames.Count; botIndex++) {
+                Console.WriteLine (botIndex + " : " + botNames[botIndex]);
             }
 
-            var botNumber = ReadNumber("Which bot would you like to download from?");
+            var botNumber = ReadNumber ("Which bot would you like to download from?");
 
-
-
-
-
-            for (int i = 0; i <= HorribleSubsPacklist.ShowEntry.GetTotalNumberOfEpisodes(shows); i++)
-            {
-                foreach (var show in shows)
-                {
-                    if (int.Parse(show.episodeNumber) == i)
-                    {
+            for (int i = 0; i <= HorribleSubsPacklist.ShowEntry.GetTotalNumberOfEpisodes (shows); i++) {
+                foreach (var show in shows) {
+                    if (int.Parse (show.episodeNumber) == i) {
                         if (show.botName != botNames[botNumber])
                             continue;
-                        if (!showOptions.Where((entry => int.Parse(entry.episodeNumber) == i)).Any())
-                        {
-                            showOptions.Add(show);
+                        if (!showOptions.Where ((entry => int.Parse (entry.episodeNumber) == i)).Any ()) {
+                            showOptions.Add (show);
                         }
                     }
 
                 }
             }
-            Download(showOptions);
+            Download (showOptions);
         }
-        public static void DownloadSomePrompt(List<HorribleSubsPacklist.ShowEntry> shows)
-        {
-        rangePrompt:
-            var episodeRangeBegin =
-                ReadNumber(
+        public static void DownloadSomePrompt (List<HorribleSubsPacklist.ShowEntry> shows) {
+            rangePrompt : var episodeRangeBegin =
+                ReadNumber (
                     $"Which episode would you like the range to begin with? (1-{HorribleSubsPacklist.ShowEntry.GetTotalNumberOfEpisodes(shows)})");
 
             var episodeRangeEnd =
-                ReadNumber(
+                ReadNumber (
                     $"Which episode would you like the range to end with? ({episodeRangeBegin}-{HorribleSubsPacklist.ShowEntry.GetTotalNumberOfEpisodes(shows)})");
 
-            if (episodeRangeEnd <= episodeRangeBegin)
-            {
-                Console.WriteLine("Episode range end must be greater than the beginning of the episode range!");
+            if (episodeRangeEnd <= episodeRangeBegin) {
+                Console.WriteLine ("Episode range end must be greater than the beginning of the episode range!");
                 goto rangePrompt;
             }
-            List<HorribleSubsPacklist.ShowEntry> showOptions = new List<HorribleSubsPacklist.ShowEntry>();
+            List<HorribleSubsPacklist.ShowEntry> showOptions = new List<HorribleSubsPacklist.ShowEntry> ();
 
-
-
-            List<string> botNames = new List<string>();
-            foreach (var showEntry in shows)
-            {
-                if (!botNames.Contains(showEntry.botName))
-                    botNames.Add(showEntry.botName);
+            List<string> botNames = new List<string> ();
+            foreach (var showEntry in shows) {
+                if (!botNames.Contains (showEntry.botName))
+                    botNames.Add (showEntry.botName);
             }
 
-            for (int botIndex = 0; botIndex < botNames.Count; botIndex++)
-            {
-                Console.WriteLine(botIndex + " : " + botNames[botIndex]);
+            for (int botIndex = 0; botIndex < botNames.Count; botIndex++) {
+                Console.WriteLine (botIndex + " : " + botNames[botIndex]);
             }
 
-            var botNumber = ReadNumber("Which bot would you like to download from?");
+            var botNumber = ReadNumber ("Which bot would you like to download from?");
 
-
-            for (int i = episodeRangeBegin; i <= episodeRangeEnd; i++)
-            {
-                foreach (var show in shows)
-                {
+            for (int i = episodeRangeBegin; i <= episodeRangeEnd; i++) {
+                foreach (var show in shows) {
                     if (show.botName != botNames[botNumber])
                         continue;
-                    if (int.Parse(show.episodeNumber) == i)
-                    {
-                        if (!showOptions.Where((entry => int.Parse(entry.episodeNumber) == i)).Any())
-                        {
-                            showOptions.Add(show);
+                    if (int.Parse (show.episodeNumber) == i) {
+                        if (!showOptions.Where ((entry => int.Parse (entry.episodeNumber) == i)).Any ()) {
+                            showOptions.Add (show);
                         }
                     }
                 }
             }
-            Download(showOptions);
-
-
+            Download (showOptions);
 
         }
-        public static void DownloadOnePrompt(List<HorribleSubsPacklist.ShowEntry> shows)
-        {
-            var episodeNumber = ReadNumber("Which episode would you like to download? (1-" +
-                                           HorribleSubsPacklist.ShowEntry.GetTotalNumberOfEpisodes(shows) + ")");
+        public static void DownloadOnePrompt (List<HorribleSubsPacklist.ShowEntry> shows) {
+            var episodeNumber = ReadNumber ("Which episode would you like to download? (1-" +
+                HorribleSubsPacklist.ShowEntry.GetTotalNumberOfEpisodes (shows) + ")");
 
-            List<HorribleSubsPacklist.ShowEntry> showOptions = new List<HorribleSubsPacklist.ShowEntry>();
-            foreach (var show in shows)
-            {
-                if (int.Parse(show.episodeNumber) == episodeNumber)
-                {
-                    showOptions.Add(show);
+            List<HorribleSubsPacklist.ShowEntry> showOptions = new List<HorribleSubsPacklist.ShowEntry> ();
+            foreach (var show in shows) {
+                if (int.Parse (show.episodeNumber) == episodeNumber) {
+                    showOptions.Add (show);
                 }
 
             }
-            for (var index = 0; index < showOptions.Count; index++)
-            {
+            for (var index = 0; index < showOptions.Count; index++) {
                 var showOption = showOptions[index];
-                Console.WriteLine($"{index} : {showOption.botName}");
+                Console.WriteLine ($"{index} : {showOption.botName}");
             }
-            var botNumber = ReadNumber("Which bot would you like to download from?");
-            Download(showOptions[botNumber]);
-
-
+            var botNumber = ReadNumber ("Which bot would you like to download from?");
+            Download (showOptions[botNumber]);
 
         }
 
-        public static void Download(HorribleSubsPacklist.ShowEntry entry)
-        {
-            if (!hasBegunDownload)
-            {
-                handler.SetDownloadDirectory(Path.Combine(Directory.GetCurrentDirectory(), entry.PrettyTitle()));
+        public static void Download (HorribleSubsPacklist.ShowEntry entry) {
+            if (!hasBegunDownload) {
+                handler.SetDownloadDirectory (Path.Combine (Directory.GetCurrentDirectory (), entry.PrettyTitle ()));
 
                 hasBegunDownload = true;
             }
 
-            handler.Download(entry.botName, int.Parse(entry.packNumber));
+            handler.Download (entry.botName, int.Parse (entry.packNumber));
         }
 
-        public static void Download(List<HorribleSubsPacklist.ShowEntry> entries)
-        {
+        public static void Download (List<HorribleSubsPacklist.ShowEntry> entries) {
 
-            foreach (var showEntry in entries)
-            {
-                Download(showEntry);
+            foreach (var showEntry in entries) {
+                Download (showEntry);
             }
         }
 
-
-
-        public static int ReadNumber(string prompt)
-        {
+        public static int ReadNumber (string prompt) {
             int result;
-            while (true)
-            {
-                Console.WriteLine(prompt);
-                if (int.TryParse(Console.ReadLine(), out result))
+            while (true) {
+                Console.WriteLine (prompt);
+                if (int.TryParse (Console.ReadLine (), out result))
                     break;
             }
 
             return result;
         }
 
-        public static void Log(string log, bool verbose = false)
-        {
-            if (!verbose)
-            {
-                Console.WriteLine(log);
-            }
-            else
-            {
+        public static void Log (string log, bool verbose = false) {
+            if (!verbose) {
+                Console.WriteLine (log);
+            } else {
                 if (verboseLogging || System.Diagnostics.Debugger.IsAttached)
-                    Console.WriteLine("[VERB] : " + log);
+                    Console.WriteLine ("[VERB] : " + log);
             }
         }
 
     }
-
 
 }
