d3f9c553253eee58aa6844bb2be4d733eb6cd314
Nathan Button <nathanebutton@gmail.com>
2019-06-27 15:10:30 -0500

    Change buildfile to use correct mac architecture

--- build.bat
+++ build.bat
@@ -18,7 +18,7 @@ cd Release
 cd netcoreapp2.2
 warp-packer -a windows-x64 -i win-x64\publish\ -e AnimeDownloaderCLI.exe -o AnimeDownloaderCLI.exe
 warp-packer -a linux-x64 -i linux-x64\publish\ -e AnimeDownloaderCLI -o AnimeDownloaderCLILinux
-warp-packer -a osx-x64 -i osx-x64\publish\ -e AnimeDownloaderCLI -o AnimeDownloaderCLIOsx
+warp-packer -a macos-x64 -i osx-x64\publish\ -e AnimeDownloaderCLI -o AnimeDownloaderCLIOsx
 7z a win-x64 AnimeDownloaderCLI.exe
 7z a linux-x64 AnimeDownloaderCLILinux
 7z a osx-x64 AnimeDownloaderCLIOsx
