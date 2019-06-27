c6c37d03331d8c77a694e7901f2e3f5bd906a792
Nathan Button <nathanebutton@gmail.com>
2019-06-27 00:20:02 -0500

    Add logic for user presence checking

--- DownloadHandler.cs
+++ DownloadHandler.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using SimpleIRCLib;
 
 namespace AnimeDown {
@@ -13,13 +16,57 @@ namespace AnimeDown {
         private readonly SimpleIRC irc;
         readonly Queue<DownloadPair> downloadQueue = new Queue<DownloadPair> ();
         private bool firstRun = true;
+        private string[] currentUsers;
         public DownloadHandler () {
             irc = new SimpleIRC ();
             irc.SetupIrc (RIZON_IRC_SERVER_HOST, IRC_USERNAME_PREFIX + new Random ().Next (100), RIZON_IRC_CHANNEL_NAME, RIZON_IRC_SERVER_PORT);
             irc.DccClient.OnDccEvent += DownloadStatusChanged;
+            irc.IrcClient.OnUserListReceived += OnUserListReceived;
             irc.SetCustomDownloadDir (System.IO.Directory.GetCurrentDirectory ()); // ! This will be overwritten later.
             irc.StartClient ();
         }
+
+        public bool IsUserPresent (string userName) {
+            if (currentUsers == null)
+                WaitForUserList ();
+            return currentUsers.Contains (userName.ToLower ());
+        }
+
+        private void WaitForUserList () {
+            Console.WriteLine ("Waiting for user list");
+            using (AutoResetEvent are = new AutoResetEvent (false)) {
+                Task task = Task.Factory.StartNew (async () => {
+                    while (currentUsers == null) {
+                        System.Console.WriteLine ("Still waiting...");
+                        await Task.Delay (5000);
+                    }
+                    are.Set ();
+                });
+                are.WaitOne ((int) TimeSpan.FromSeconds (30).TotalMilliseconds);
+                if (currentUsers == null)
+                    throw new Exception ("Never recieved a user list!");
+                Console.WriteLine ("User list found.");
+            }
+        }
+
+        private void OnUserListReceived (object sender, IrcUserListReceivedEventArgs e) {
+            currentUsers = e.UsersPerChannel
+                .Where (
+                    (s) => s.Key.ToLower () == RIZON_IRC_CHANNEL_NAME.ToLower ()
+                )
+                .First ()
+                .Value.Select (
+                    (s) => s.ToLower ()
+                )
+                .Where (
+                    (s) => s.StartsWith ('%')
+                )
+                .Select (
+                    (s) => s.TrimStart ('%')
+                )
+                .ToArray ();
+        }
+
         private void DownloadStatusChanged (object sender, DCCEventArgs args) {
             Console.Clear ();
             Console.WriteLine ($"Current File: {args.FileName} ");
