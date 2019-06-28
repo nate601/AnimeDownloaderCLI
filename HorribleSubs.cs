a8356cd10c254f1f91dcef26797c02412bb91cc7
Nathan Button <nathanebutton@gmail.com>
2019-06-27 23:27:04 -0500

    Change order of episode selection
    An issue was brought to my attention wherein if an episode is not
    available in the selected quality, the user would not know that it
    existed at all.  This is made evident in the example of the PsychoPass
    first season (which is only available in 720p).  This commit changes the
    order that information is presented to the user, showing all the
    shows followed by the qualities that these shows are available in.

--- HorribleSubs.cs
+++ HorribleSubs.cs
@@ -5,7 +5,7 @@ using System.Net;
 
 public partial class HorribleSubsPacklist
 {
-    public List<ShowEntry> GetShow(string title, Quality quality)
+    public List<ShowEntry> GetShow(string title)
     {
         string url = "https://xdcc.horriblesubs.info/search.php" + "?t=" + title + "";
         HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
@@ -24,25 +24,7 @@ public partial class HorribleSubsPacklist
         List<ShowEntry> shows = new List<ShowEntry>();
         foreach (var s in response.Split(Environment.NewLine.ToCharArray()))
         {
-            switch (quality)
-            {
-                case Quality.TEN_EIGHTY_P:
-                    if (s.Contains("1080p"))
-                    {
-                        shows.Add(new ShowEntry(s));
-                    }
-                    break;
-                case Quality.SEVEN_TWENTY_P:
-                    if (s.Contains("720p"))
-                    {
-                        shows.Add(new ShowEntry(s));
-                    }
-                    break;
-                case Quality.STANDARD_DEFINITION:
-                    if (!s.Contains("1080p") && !s.Contains("720p"))
-                        shows.Add(new ShowEntry(s));
-                    break;
-            }
+            shows.Add(new ShowEntry(s));
         }
 
         var showVerified = new List<ShowEntry>();
