9ae61a757a6838c108c1a6b0d0c33f6656959f69
Nathan Button <nathanebutton@gmail.com>
2019-11-17 01:59:31 +0000

    Update the rendering on the episode display prettified the episode availability display.

--- Program.cs
+++ Program.cs
@@ -99,22 +99,35 @@ namespace AnimeDown
         private static string ChooseBotPrompt(List<HorribleSubsPacklist.ShowEntry> shows)
         {
             List<string> botNames = new List<string>();
-	    Dictionary<string, int> botNameShowCount = new Dictionary<string, int>();
-            foreach (var showEntry in shows)
+            int GetTotalNumberOfEpisodes = HorribleSubsPacklist.ShowEntry.GetTotalNumberOfEpisodes(shows);
+	    foreach (var showEntry in shows)
             {
                 if (!botNames.Contains(showEntry.botName)) { 
-		    botNameShowCount.Add(showEntry.botName, 0);
 		    botNames.Add(showEntry.botName);
 		}
-		botNameShowCount[showEntry.botName]++;
             }
             for (int i = 0; i < botNames.Count; i++)
             {
+		int botNumberOfEpisodes = 0;
                 bool botIsOnline = handler.Value.IsUserPresent(botNames[i]);
                 Console.Write($"{i} : ");
                 WriteInColor($"{botNames[i]} [{(botIsOnline ? "ON" : "OFF")}LINE]", botIsOnline ? ConsoleColor.Green : ConsoleColor.Red);
                 Console.Write("\n");
-		Console.WriteLine($"::Episode Count {botNameShowCount[botNames[i]]}");
+		Console.Write("|");
+		for (int j = 1; j <=  GetTotalNumberOfEpisodes; j++)
+		{
+		    if(shows.Where((x)=>(x.botName==botNames[i])).Any((x)=>(int.Parse(x.episodeNumber) == j)))
+		    {
+			WriteInColor($"{j}", ConsoleColor.Green);
+			botNumberOfEpisodes++;
+		    }else{
+			    
+			WriteInColor($"{j}", ConsoleColor.Red);
+		    }
+		    Console.Write("|");		    
+		}
+		Console.WriteLine($"\n {botNumberOfEpisodes}/{GetTotalNumberOfEpisodes}");
+		
             }
             var botNumber = ReadNumber("Which bot would you like to download from?", botNames.Count - 1);
 
