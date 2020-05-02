bacb5575366f73fe9e8e4ebd0c50371b054d3d6d
Nathan Button <nathanebutton@gmail.com>
2020-05-02 01:40:05 -0500

    Update winBuild.bat

--- winBuild.bat
+++ winBuild.bat
@@ -2,7 +2,7 @@
 dotnet publish -r win-x64 --self-contained true -c Release
 dotnet publish -r linux-x64 --self-contained true -c Release
 dotnet publish -r osx-x64 --self-contained true -c Release
-dotnet publish -r win-x64 --self-contained false -c Release -o bin\Release\netcoreapp2.2\win-x64-dependent\publish
+dotnet publish -r win-x64 --self-contained false -c Release -o bin\Release\netcoreapp3.1\win-x64-dependent\publish
 
 windows-x64.warp-packer.exe -a windows-x64 -i bin\Release\netcoreapp3.1\win-x64\publish -e AnimeDownloaderCLI.exe -o AnimeDownloaderCLI.exe
 windows-x64.warp-packer.exe -a linux-x64 -i bin\Release\netcoreapp3.1\linux-x64\publish\ -e AnimeDownloaderCLI -o AnimeDownloaderCLILinux
